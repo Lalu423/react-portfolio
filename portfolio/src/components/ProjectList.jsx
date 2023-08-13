cae4f6ad4937d1352529fbba5500e7c0f05b2c43
Lalu423 <>
2023-08-12 23:06:58 -0400

    created framework

---
+++ portfolio/src/components/ProjectList.jsx
@@ -0,0 +1,33 @@
+export default function ProjectList(){
+  const projs = [
+    {
+      name: "Proj 1",
+      link: "https://sarroyo551.github.io/Activity-Roulette/"
+    },
+    {
+      name: "Proj 2",
+      link: "https://workout-plus-c7901a67a230.herokuapp.com/"
+    },
+    {
+      name: "Proj 3",
+      link: "https://workout-plus-c7901a67a230.herokuapp.com/"
+    }
+  ]
+
+  return (
+    <section>
+      <h2>Projects</h2>
+      <ul>
+      {
+        projs.map((project, i) => (
+          <li key = {i}>
+            <a href={project.link}>
+              {project.name}
+            </a>
+          </li>
+        ))
+      }
+      </ul>
+    </section>
+  )
+};
